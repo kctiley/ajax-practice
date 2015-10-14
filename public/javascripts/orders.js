6795817ad2fee82d8f0375bd2e984bc07e2ff93a
Klint Tiley <kctiley@gmail.com>
2015-10-13 19:05:05 -0600

    Fixed bug with adding new.

--- public/javascripts/orders.js
+++ public/javascripts/orders.js
@@ -1,15 +1,57 @@
 
 $(document).ready(function(){
 
+  console.log('document loaded')
+
+  //Cach orders, name, and drink
+  var $orders = $('#listOfOrders');
+  var $inputName = $('#inputName');
+  var $inputDrink = $('#inputDrink');
+  var addOrder= function(input){
+    $orders.append('<div style=\'padding: 5px; border: 2px solid blue; width: 25%\'><ul> Name: ' + input.name  + '</ul><ul> Drink: ' + input.drink + '</ul></div>');
+  }
+   
+
+  //Show database orders on page load
   $.ajax({
     type: 'GET',
     url: '/orders',
     success: function(orders){
       $.each(orders, function(i, order){
-        $('#listOfOrders').append('<li> Name: ' + order.name  + '</li><ul> Drink: ' + order.drink + '</ul>');
+        addOrder(order);
       })
+    },
+    error: function(){
+      alert('Error loading orders')
     }
   })
  
+  //Add order
+  $('#btnAdd').on('click', function(){
+
+    console.log('btnAdd has been clicked')
+
+    var order = {
+      name: $inputName.val(),
+      drink: $inputDrink.val()
+    };
+
+    $.ajax({
+
+      type: 'POST',
+      url: '/orders',
+      data: order,
+      success: function(newOrder){
+        addOrder(order);
+      },
+      error: function(){
+        alert('Error adding order')
+      },        
+
+    })
+    
+
+
+   })
 
 })
--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,19 @@ router.get('/orders', function(req, res) {
     });
 });
 
+/*
+ * POST to adduser.
+ */
+router.post('/orders', function(req, res) {
+    var db = req.db;
+    var collection = db.get('orders');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+
 
 module.exports = router;
